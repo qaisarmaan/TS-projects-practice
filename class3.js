f5602c3ccf99a3921018d86f33af623f4d04c773
Qaisar Maan <bakhtawar04@yahoo.com>
2023-08-05 20:34:07 +0500

    1 to 10

--- class3.js
+++ class3.js
@@ -1,14 +1,73 @@
-//console.log("hello, Qaisar would you like to learn typescript.");
-//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-var personName = "qaisar Maan";
-// Convert the name to lowercase
-var lowercaseName = personName.toLowerCase();
-// Convert the name to uppercase
-var uppercaseName = personName.toUpperCase();
-// Convert the name to title case
-var titlecaseName = personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); });
-// Printing the results
-console.log("Original Name: " + personName);
-console.log("Lowercase Name: " + lowercaseName);
-console.log("Uppercase Name: " + uppercaseName);
-console.log("Titlecase Name: " + titlecaseName);
+//exercise 01
+// I have installed Node.js, TypeScript and VS Code on my computer.
+
+//-----------------------------------
+
+//exercise 02
+const personeName1 = "Qaiser";
+console.log(`Hello ${personeName1}, would you like to learn some AI today?`);
+
+//-----------------------------------
+
+//exercise 03
+const personeName = "Qaiser";
+console.log(personeName.toLowerCase());
+console.log(personeName.toUpperCase());
+console.log(personeName.toLocaleLowerCase());
+
+//-----------------------------------
+
+//exercise 04
+const author = "Albert Einstein";
+const quote = "A person who never made a mistake never tried anything new.";
+console.log(`${author} once said, "${quote}"`);
+
+//-----------------------------------
+
+//exercise 05
+const famousPerson = "Albert Einstein";
+const message = `${famousPerson} once said, "A person who never made a mistake never tried anything new."`;
+console.log(message);
+
+//-----------------------------------
+
+//exercise 06
+const pname = "\t  Qaiser Maan \n";
+console.log(pname);
+console.log(pname.trim());
+
+//-----------------------------------
+
+//exercise 07
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+
+//-----------------------------------
+
+//exercise 09
+const favoriteNumber = 1;
+console.log(`My favorite number is ${favoriteNumber}.`);
+
+//-----------------------------------
+
+//exercise 10
+const comment = 'Qaiser';
+console.log(`Hello, ${comment}!`)
+
+
+// //console.log("hello, Qaisar would you like to learn typescript.");
+// //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// var personName = "qaisar Maan";
+// // Convert the name to lowercase
+// var lowercaseName = personName.toLowerCase();
+// // Convert the name to uppercase
+// var uppercaseName = personName.toUpperCase();
+// // Convert the name to title case
+// var titlecaseName = personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); });
+// // Printing the results
+// console.log("Original Name: " + personName);
+// console.log("Lowercase Name: " + lowercaseName);
+// console.log("Uppercase Name: " + uppercaseName);
+// console.log("Titlecase Name: " + titlecaseName);
